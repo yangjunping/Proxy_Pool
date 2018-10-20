ab1bea5bdf0210df7c50dfa2997e63edfa7fc04d
yangjunping <877498362@qq.com>
2018-10-20 20:57:32 +0800

    modfied

--- TestProxy.py
+++ TestProxy.py
@@ -5,17 +5,19 @@ from redis_operate import redis_operate
 class TestProxy():
     def __init__(self):
         self.redislink = redis_operate()
-    def detection(self,proxy):
+    def detection(self,proxy):          #检测装置，测试百度，获取返回码判断
         # proxies={
         #     'http':'http://'+proxy,
         #     'https':'https://'+proxy,
         # }
         res=requests.get("https://www.baidu.com",proxy,timeout=10)
         if(res.status_code==200):
-            self.redislink.add(proxy)
+            # self.redislink.add(proxy)
             print("有效代理",proxy)
+            return True
         else:
             print("不是有效代理")
+            return False
 #         conn=aiohttp.TCPConnector(verify_ssl=False)
 #         async with aiohttp.ClientSession(connnector=conn) as session:
 #             async with session.get("https://www.baidu.com") as res:
@@ -25,16 +27,42 @@ class TestProxy():
 #                 else:
 #                 #   redis_operate.decrase(proxy)
 #                     print("不是有效IP")
+    def byte_to_utf(self,proxy_list):   #redis存储的byte方式转换为utf-8,列表返回
+        for i in proxy_list:
+            if isinstance(i, bytes):
+                i = i.decode("utf-8")
+                pro = 'http://' + i
+                yield pro
+            # ptest.detection(pro)
+            # time.sleep(2)
+    def first_insert(self,redislink):         #第一次插入全部获取的代理
+        pro = Get_Proxies()
+        proxy_list = pro.crawl_xici()
+        proxy_list=self.byte_to_utf(proxy_list)
+        #    loop=asyncio.get_event_loop()
+        for i in proxy_list:
+            if self.detection(i):
+                redislink.add(i)
+
+    def exit_test(self,redislink):                        #在已经获取的数据库里获取前三位检测
+        top3_proxy=redislink.zrevrange("proxy",0,2)
+        top3_list=self.byte_to_utf(top3_proxy)
+        for i in top3_list:
+            if self.detection(i):
+                print("代理可用")
+            else:
+                self.redislink.decrease(i)
+
 if __name__ == '__main__':
-    pro=Get_Proxies()
-    proxy_list=pro.crawl_xici()
-    ptest=TestProxy()
-#    loop=asyncio.get_event_loop()
-    for i in proxy_list:
-        if isinstance(i,bytes):
-            i=i.decode("utf-8")
-        pro='http://'+i
-        ptest.detection(pro)
-        time.sleep(2)
+    redislink=redis_operate()
+    redis=redislink.getredis()
+    testproxt=TestProxy()
+    if(redislink.isexit(redis)):
+        print("redis已经有现成的代理，尽情享用吧！")
+        testproxt.exit_test(redis)
+    else:
+        first_insert()
+
+
 
 
--- redis_operate.py
+++ redis_operate.py
@@ -1,7 +1,8 @@
 from redis import StrictRedis
 class redis_operate():
-    def __init__(self):
+    def getredis(self):
         self.redis=StrictRedis(host="localhost",port=6379,db=0)
+        return self.redis
     def add(self,value):
         self.redis.zadd("proxy",100,value)
     def decrease(self,value):
@@ -10,5 +11,11 @@ class redis_operate():
         self.redis.zincrby("proxy", value, 1)
     def delete(self,value):
         self.redis.zremrangebyscore("proxy",0,10)
+    def isexit(self,redis):
+        b=self.redis.zcard('proxy')
+        if b>0:
+            return True
+        else:
+            return False
 
 
